6a30df8078c67a8849f5cb71cb3ac5f5c626ca40
YoursTrulyAdi <adityasahu5891@gmail.com>
2025-09-03 19:40:54 +0530

    Initial changes

--- frontend_files/assets/js/main.js
+++ frontend_files/assets/js/main.js
@@ -1,2 +1,29 @@
 //fetching and DOM updates
 console.log("Hello World");
+
+const arrow = document.querySelector("#arrow");
+const target = document.querySelector("#target");
+const loginBtn = document.querySelector(".login-btn");
+const password = document.querySelector("#password");
+const toggle = document.querySelector("#togglePassword");
+
+//event listener
+arrow.addEventListener("click", () => {
+    target.scrollIntoView({
+        behavior: "smooth"
+    })
+})
+
+loginBtn.addEventListener("click", () => {
+    window.location.href = "/frontend_files/userLogin.html";
+})
+
+
+toggle.addEventListener("click", () => {
+  // toggle type
+  const type = password.getAttribute("type") === "password" ? "text" : "password";
+  password.setAttribute("type", type);
+  
+  // optionally toggle icon
+  toggle.textContent = type === "password" ? "👁️" : "🙈";
+});
